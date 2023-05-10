e0bca0a1a078662d2fef562f844e16905bb60359
Mallika <kmallika.malli@gmail.com>
2023-05-10 17:33:40 +0530

    shell script

---
+++ redis.sh
@@ -0,0 +1,10 @@
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf module enable redis:remi-6.2 -y
+
+yum install redis -y
+
+## enable the listen address from 127.0.0.0 to 0.0.0.0
+
+
+systemctl enable redis
+systemctl start redis
