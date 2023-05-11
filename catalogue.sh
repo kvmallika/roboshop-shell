ae0ea32e32e986d8f91096f214e985b8753bb9e5
Mallika <kmallika.malli@gmail.com>
2023-05-11 19:07:26 +0530

    changing the path of catalogue.service file

--- catalogue.sh
+++ catalogue.sh
@@ -1,41 +1,41 @@
 
-echo -e "\e[33m ***** NoteJS repo files ***** \e[33m"
+echo -e "\e[31m ***** NoteJS repo files ***** \e[31m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[33m ***** Install NoteJS ***** \e[33m"
+echo -e "\e[31m ***** Install NoteJS ***** \e[31m"
 yum install nodejs -y
 
-echo -e "\e[33m ***** adding application user ***** \e[33m"
+echo -e "\e[31m ***** adding application user ***** \e[31m"
 useradd roboshop
 
-echo -e "\e[33m ***** creating app directory ***** \e[33m"
+echo -e "\e[31m ***** creating app directory ***** \e[31m"
 rm -rf /app
 mkdir /app
 
-echo -e "\e[33m ***** downloding application code ***** \e[33m"
+echo -e "\e[31m ***** downloding application code ***** \e[31m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 
-echo -e "\e[33m ***** unzip the app content ***** \e[33m"
+echo -e "\e[31m ***** unzip the app content ***** \e[31m"
 unzip /tmp/catalogue.zip
 cd /app
 
-echo -e "\e[33m ***** installing dependencies ***** \e[33m"
+echo -e "\e[31m ***** installing dependencies ***** \e[31m"
 npm install
 
-echo -e "\e[33m ***** coping catalogue service files ***** \e[33m"
-cp \home\centos\roboshop-shell\catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[31m ***** coping catalogue service files ***** \e[31m"
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
-echo -e "\e[33m ***** starting catalogue service ***** \e[33m"
+echo -e "\e[31m ***** starting catalogue service ***** \e[31m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
-echo -e "\e[33m ***** coping mongodb repo files ***** \e[33m"
+echo -e "\e[31m ***** coping mongodb repo files ***** \e[31m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[33m ***** installing mongodb client ***** \e[33m"
+echo -e "\e[31m ***** installing mongodb client ***** \e[31m"
 yum install mongodb-org-shell -y
 
-echo -e "\e[33m ***** load schema ***** \e[33m"
+echo -e "\e[31m ***** load schema ***** \e[31m"
 mongo --host mongodb-dev.vemdevops.online </app/schema/catalogue.js
