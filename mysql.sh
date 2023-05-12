8063c2b585beff03f3ec35259d1b90dd1354dae0
Mallika <kmallika.malli@gmail.com>
2023-05-12 10:34:35 +0530

    updated the cart and mysql scripts

--- mysql.sh
+++ mysql.sh
@@ -14,7 +14,4 @@ systemctl start mysqld
 echo -e "\e[31m ***** setting the root password ***** \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 
-echo -e "\e[31m ***** enabling the root password ***** \e[0m"
-mysql -uroot -pRoboShop@1
-
 
