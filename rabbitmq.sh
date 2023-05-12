e4cd235d49f8146dd04a46c2ffac3333fe028212
Mallika <kmallika.malli@gmail.com>
2023-05-12 10:48:56 +0530

    updated the shipping and rabbitmq scripts

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,11 +1,20 @@
+echo -e "\e[31m ***** config. YUM repos ***** \e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+echo -e "\e[31m ***** Install Erlang ***** \e[0m"
 yum install erlang -y
+
+echo -e "\e[31m ***** Configure YUM repos for RabbitMQ ***** \e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+echo -e "\e[31m ***** Install RabbitMQ ***** \e[0m"
 yum install rabbitmq-server -y
 
+echo -e "\e[31m ***** Start RabbitMQ service ***** \e[0m"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
+echo -e "\e[31m ***** Creating user for the application ***** \e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
--- shipping.sh
+++ shipping.sh
@@ -1,23 +1,35 @@
+echo -e "\e[31m ***** install maven ***** \e[0m"
 yum install maven -y
+
+echo -e "\e[31m ***** adding application user ***** \e[0m"
 useradd roboshop
+
+echo -e "\e[31m ***** setting app directory ***** \e[0m"
+rm -rf /app
 mkdir /app
 
+echo -e "\e[31m ***** downloading the app code and unzipping it ***** \e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
 unzip /tmp/shipping.zip
-
 cd /app
+
+echo -e "\e[31m ***** download the dependencies & build the application ***** \e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
-cp shipping.service /etc/systemd/system/shipping.service
+echo -e "\e[31m ***** setting up the systemD files ***** \e[0m"
+cp //home//centos//roboshop-shell//shipping.service /etc/systemd/system/shipping.service
 
+echo -e "\e[31m ***** enable and starting the shipping service  ***** \e[0m"
 systemctl daemon-reload
 
 systemctl enable shipping
 systemctl start shipping
 
+echo -e "\e[31m ***** install MySQL client ***** \e[0m"
 yum install mysql -y
-mysql -h mysql-dev.vemdevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+echo -e "\e[31m ***** Load Schema ***** \e[0m"
+mysql -h mysql-dev.vemdevops.online -uroot -pRoboShop@1 </app/schema/shipping.sql
 
 systemctl restart shipping
