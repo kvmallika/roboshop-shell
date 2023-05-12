446ea2945fb285b52bfb4e2893f1163ca5fe922a
Mallika <kmallika.malli@gmail.com>
2023-05-12 09:35:26 +0530

    changed the mongodb repo files path

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,8 @@
 
-echo -e "\e[31m ***** NoteJS repo files ***** \e[0m"
+echo -e "\e[31m ***** NodeJS repo files ***** \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[31m ***** Install NoteJS ***** \e[0m"
+echo -e "\e[31m ***** Install NodeJS ***** \e[0m"
 yum install nodejs -y
 
 echo -e "\e[31m ***** adding application user ***** \e[0m"
@@ -12,7 +12,7 @@ echo -e "\e[31m ***** creating app directory ***** \e[0m"
 rm -rf /app
 mkdir /app
 
-echo -e "\e[31m ***** downloding application code ***** \e[0m"
+echo -e "\e[31m ***** downloading application code ***** \e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 
@@ -23,7 +23,6 @@ cd /app
 echo -e "\e[31m ***** installing dependencies ***** \e[0m"
 npm install
 
-echo "test"
 echo -e "\e[31m ***** coping catalogue service files ***** \e[0m"
 cp //home//centos//roboshop-shell//catalogue.service  /etc/systemd/system/catalogue.service
 
