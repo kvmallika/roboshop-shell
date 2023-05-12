46a95126096e574d21e327d6e594b539254c0816
Mallika <kmallika.malli@gmail.com>
2023-05-12 09:53:59 +0530

    updated the redis and user scripts

--- redis.sh
+++ redis.sh
@@ -1,10 +1,16 @@
+
+echo -e "\e[31m ***** install redis repo files ***** \e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+cho -e "\e[31m ***** enable the packages ***** \e[0m"
 dnf module enable redis:remi-6.2 -y
 
+echo -e "\e[31m ***** Install redis ***** \e[0m"
 yum install redis -y
 
-## enable the listen address from 127.0.0.0 to 0.0.0.0
+echo -e "\e[31m ***** updating the listen address ***** \e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
-
+echo -e "\e[31m ***** start and enable redis service ***** \e[0m"
 systemctl enable redis
 systemctl start redis
--- user.sh
+++ user.sh
@@ -1,17 +1,40 @@
+echo -e "\e[31m ***** NodeJS repo files ***** \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[31m ***** Install NodeJS ***** \e[0m"
 yum install nodejs -y
+
+echo -e "\e[31m ***** adding application user ***** \e[0m"
 useradd roboshop
+
+echo -e "\e[31m ***** creating app directory ***** \e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[31m ***** downloading application code ***** \e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
+
+echo -e "\e[31m ***** unzip the app content ***** \e[0m"
 unzip /tmp/user.zip
 cd /app
+
+echo -e "\e[31m ***** installing dependencies ***** \e[0m"
 npm install
-cp user.service /etc/systemd/system/user.service
+
+cho -e "\e[31m ***** coping user service files ***** \e[0m"
+cp //home//centos//roboshop-shell//user.service /etc/systemd/system/user.service
+
+echo -e "\e[31m ***** starting user service ***** \e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[31m ***** coping mongodb repo files ***** \e[0m"
+cp //home//centos//roboshop-shell//mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[31m ***** installing mongodb client ***** \e[0m"
 yum install mongodb-org-shell -y
+
+echo -e "\e[31m ***** load schema ***** \e[0m"
 mongo --host mongodb-dev.vemdevops.online /app/schema/user.js
