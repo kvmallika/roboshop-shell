bf7194e2c7ea38c907d243c5edbd471448550886
Mallika <kmallika.malli@gmail.com>
2023-05-12 10:35:07 +0530

    updated the mysql scripts

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,7 @@ yum install mysql-community-server -y
 
 echo -e "\e[31m ***** enable and start MySQL service ***** \e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 
 echo -e "\e[31m ***** setting the root password ***** \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
