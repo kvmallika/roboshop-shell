bb58ba37bce009edc3749b39ab688d4e64de6c34
Mallika <kmallika.malli@gmail.com>
2023-05-12 11:03:52 +0530

    updated the dispatch and payment scripts

--- dispatch.sh
+++ dispatch.sh
@@ -1,17 +1,30 @@
+
+echo -e "\e[31m ***** Installing Python  ***** \e[0m"
 yum install golang -y
+
+echo -e "\e[31m ***** adding application user ***** \e[0m"
 useradd roboshop
 
+echo -e "\e[31m ***** creating the app directory ***** \e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[31m ***** downloading and unzipping the application code  ***** \e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
 unzip /tmp/dispatch.zip
 
 cd /app
+
+echo -e "\e[31m ***** Download the dependencies ***** \e[0m"
 go mod init dispatch
 go get
 go build
 
-cp dispatch.service /etc/systemd/system/dispatch.service
+echo -e "\e[31m ***** setup systemD dispatch service ***** \e[0m"
+cp //home//centos//roboshop-shell//dispatch.service /etc/systemd/system/dispatch.service
+
+echo -e "\e[31m ***** start and enable dispatch service ***** \e[0m"
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl start dispatch
--- payment.sh
+++ payment.sh
@@ -1,16 +1,26 @@
+echo -e "\e[31m ***** Installing Python  ***** \e[0m"
 yum install python36 gcc python3-devel -y
+
+echo -e "\e[31m ***** adding application user ***** \e[0m"
 useradd roboshop
+
+echo -e "\e[31m ***** creating the app directory ***** \e[0m"
+rm -rf /app
 mkdir /app
 
+echo -e "\e[31m ***** downloading and unzipping the application code  ***** \e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
 unzip /tmp/payment.zip
 
+echo -e "\e[31m ***** Download the dependencies ***** \e[0m"
 cd /app
 pip3.6 install -r requirements.txt
 
-cp payment.service /etc/systemd/system/payment.service
+echo -e "\e[31m ***** setup systemD payment service ***** \e[0m"
+cp //home//centos//roboshop-shell//payment.service /etc/systemd/system/payment.service
 
+echo -e "\e[31m ***** start and enable payment service ***** \e[0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
