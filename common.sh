55d4e2ce2e87488255879c611d105035a84c849a
Mallika <kmallika.malli@gmail.com>
2023-05-15 13:34:17 +0530

    changes in the shipping script

--- common.sh
+++ common.sh
@@ -14,6 +14,39 @@ func_schema_setup() {
     func_print_head "load schema"
     mongo --host mongodb-dev.vemdevops.online </app/schema/${component}.js
  fi
+ if [ "schema_setup" == "mysql" ]; then
+     func_print_head "install MySQL client"
+     yum install mysql -y
+
+     func_print_head "Load Schema"
+     mysql -h mysql-dev.vemdevops.online -uroot -${mysql_root_password} < /app/schema/${component}.sql
+  fi
+ }
+
+func_app_prereq()
+{
+    func_print_head "adding application user"
+    useradd ${app_user}
+
+    func_print_head "setting app directory"
+    rm -rf /app
+    mkdir /app
+
+    func_print_head "downloading the app code and unzipping it"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    cd /app
+    unzip /tmp/${component}.zip
+
+}
+func_systemd_setup(){
+    func_print_head "coping catalogue service files"
+    cp ${script_path}//${component}.service  /etc/systemd/system/${component}.service
+
+    func_print_head "starting catalogue service"
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl restart ${component}
+
 }
 func_nodejs()
 {
@@ -23,32 +56,26 @@ func_nodejs()
   func_print_head "Install NodeJS"
   yum install nodejs -y
 
-  func_print_head "adding application user"
-  useradd ${app_user}
-
-  func_print_head "creating app directory"
-  rm -rf /app
-  mkdir /app
-
-  func_print_head "downloading application code"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-
-  func_print_head "unzip the app content"
-  unzip /tmp/${component}.zip
-  cd /app
+  func_app_prereq
 
   func_print_head "installing dependencies"
   npm install
+  func_schema_setup
+  func_systemd_setup
 
-  func_print_head "coping catalogue service files"
-  cp ${script_path}//${component}.service  /etc/systemd/system/${component}.service
+}
+func_java()
+{
+  func_print_head "install maven"
+  yum install maven -y
 
-  func_print_head "starting catalogue service"
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+  func_app_prereq
 
-func_schema_setup
+  func_print_head "download the dependencies & build the application"
+  mvn clean package
+  mv target/shipping-1.0.jar shipping.jar
 
+
+  func_schema_setup
+  func_systemd_setup
 }
--- shipping.sh
+++ shipping.sh
@@ -2,43 +2,11 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
+component="shipping"
+schema_setup="mysql"
 if[ -z "$mysql_root_password" ]; then
   echo input MySQL root password is missing
   exit
 fi
 
-echo -e "\e[31m ***** install maven ***** \e[0m"
-yum install maven -y
-
-echo -e "\e[31m ***** adding application user ***** \e[0m"
-useradd ${app_user}
-
-echo -e "\e[31m ***** setting app directory ***** \e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[31m ***** downloading the app code and unzipping it ***** \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-
-echo -e "\e[31m ***** download the dependencies & build the application ***** \e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[31m ***** setting up the systemD files ***** \e[0m"
-cp ${script_path}//shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[31m ***** enable and starting the shipping service  ***** \e[0m"
-systemctl daemon-reload
-
-systemctl enable shipping
-systemctl start shipping
-
-echo -e "\e[31m ***** install MySQL client ***** \e[0m"
-yum install mysql -y
-echo -e "\e[31m ***** Load Schema ***** \e[0m"
-mysql -h mysql-dev.vemdevops.online -uroot -${mysql_root_password} </app/schema/shipping.sql
-
-systemctl restart shipping
+func_java
