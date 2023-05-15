e24c40b3cf91294cccbfb4121db28eca8eed2245
Mallika <kmallika.malli@gmail.com>
2023-05-15 18:14:56 +0530

    python func created

--- common.sh
+++ common.sh
@@ -1,6 +1,10 @@
 app_user=roboshop
-func_print_head(){
+log_file=/tmp/roboshop.log
+
+func_print_head()
+{
   echo -e "\e[34m ***** $1 ***** \e[0m"
+  echo -e "\e[34m ***** $1 ***** \e[0m" &>>log_file
 }
 func_stat_check(){
   if [ $1 -eq 0 ]; then
@@ -14,23 +18,24 @@ func_schema_setup() {
   if [ "schema_setup" == "mongo" ]; then
 
     func_print_head "coping mongodb repo files"
-    cp ${script_path}//mongo.repo /etc/yum.repos.d/mongo.repo
+    cp ${script_path}//mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
     func_stat_check $?
 
     func_print_head "installing mongodb client"
-    yum install mongodb-org-shell -y
+    yum install mongodb-org-shell -y &>>$log_file
     func_stat_check $?
 
     func_print_head "load schema"
-    mongo --host mongodb-dev.vemdevops.online </app/schema/${component}.js
+    mongo --host mongodb-dev.vemdevops.online </app/schema/${component}.js &>>$log_file
     func_stat_check $?
  fi
  if [ "schema_setup" == "mysql" ]; then
      func_print_head "install MySQL client"
-     yum install mysql -y
+     yum install mysql -y &>>$log_file
+     func_stat_check $?
 
      func_print_head "Load Schema"
-     mysql -h mysql-dev.vemdevops.online -uroot -${mysql_root_password} < /app/schema/${component}.sql
+     mysql -h mysql-dev.vemdevops.online -uroot -${mysql_root_password} < /app/schema/${component}.sql &>>$log_file
      func_stat_check $?
   fi
  }
@@ -38,21 +43,28 @@ func_schema_setup() {
 func_app_prereq()
 {
     func_print_head "adding application user"
-    useradd ${app_user}
+    id ${app_user} &>>$log_file
+
+    if [ $1 -ne 0 ]; then
+      useradd ${app_user}
+    fi
+    func_stat_check $?
 
     func_print_head "setting app directory"
     rm -rf /app
     mkdir /app
 
     func_print_head "downloading the app code and unzipping it"
-    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+    func_stat_check $?
     cd /app
-    unzip /tmp/${component}.zip
+    unzip /tmp/${component}.zip &>>$log_file
+    func_stat_check $?
 
 }
 func_systemd_setup(){
     func_print_head "coping catalogue service files"
-    cp ${script_path}//${component}.service  /etc/systemd/system/${component}.service
+    cp ${script_path}//${component}.service  /etc/systemd/system/${component}.service &>>$log_file
     func_stat_check $?
 
     func_print_head "starting catalogue service"
@@ -64,17 +76,17 @@ func_systemd_setup(){
 func_nodejs()
 {
   func_print_head "NodeJS repo files"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
   func_stat_check $?
 
   func_print_head "Install NodeJS"
-  yum install nodejs -y
+  yum install nodejs -y &>>$log_file
   func_stat_check $?
 
   func_app_prereq
 
   func_print_head "installing dependencies"
-  npm install
+  npm install &>>$log_file
   func_stat_check $?
 
   func_schema_setup
@@ -84,18 +96,35 @@ func_nodejs()
 func_java()
 {
   func_print_head "install maven"
-  yum install maven -y
+  yum install maven -y &>>$log_file
   func_stat_check $?
 
   func_app_prereq
 
   func_print_head "download the dependencies & build the application"
   mvn clean package
-  mv target/${component}-1.0.jar ${component}.jar
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
   func_stat_check $?
 
-
   func_schema_setup
+  func_systemd_setup
+}
+func_python(){
+
+  func_print_head "Installing Python"
+  yum install python36 gcc python3-devel -y &>>$log_file
+  func_stat_check $?
+
+  func_app_prereq
+
+  func_print_head "Download the dependencies"
+  cd /app
+  pip3.6 install -r requirements.txt &>>$log_file
+  func_stat_check $?
+
+  func_print_head "update passwords in systemD service file"
+  sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}//payment.service &>>$log_file
+  func_stat_check $?
 
   func_systemd_setup
 
--- payment.sh
+++ payment.sh
@@ -7,30 +7,6 @@ if [ -z "$rabitmq_appuser_password" ]; then
   echo input RabbitMQ appuser password is missing
   exit
 fi
-echo -e "\e[31m ***** Installing Python  ***** \e[0m"
-yum install python36 gcc python3-devel -y
 
-echo -e "\e[31m ***** adding application user ***** \e[0m"
-useradd ${app_user}
-
-echo -e "\e[31m ***** creating the app directory ***** \e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[31m ***** downloading and unzipping the application code  ***** \e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
-cd /app
-unzip /tmp/payment.zip
-
-echo -e "\e[31m ***** Download the dependencies ***** \e[0m"
-cd /app
-pip3.6 install -r requirements.txt
-
-echo -e "\e[31m ***** setup systemD payment service ***** \e[0m"
-sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}//payment.service
-cp ${script_path}//payment.service /etc/systemd/system/payment.service
-
-echo -e "\e[31m ***** start and enable payment service ***** \e[0m"
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+component="payment"
+func_python
