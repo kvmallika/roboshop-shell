5d671fd6a2a656e2de3b2da62ac747388a41c63e
Mallika <kmallika.malli@gmail.com>
2023-05-15 13:41:19 +0530

    changes in the if condition

--- shipping.sh
+++ shipping.sh
@@ -4,7 +4,7 @@ source ${script_path}/common.sh
 mysql_root_password=$1
 component="shipping"
 schema_setup="mysql"
-if[ -z "$mysql_root_password" ]; then
+if [ -z "$mysql_root_password" ]; then
   echo input MySQL root password is missing
   exit
 fi
