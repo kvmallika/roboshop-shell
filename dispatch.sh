c010b0edd4ec0683425eaa33517bf9f7a5d44729
Mallika <kmallika.malli@gmail.com>
2023-05-16 11:36:55 +0530

    changes in dispatch

--- dispatch.sh
+++ dispatch.sh
@@ -2,32 +2,18 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[31m ***** Installing Python  ***** \e[0m"
+component="dispatch"
+
+func_print_head "Installing Python"
 yum install golang -y
 
-echo -e "\e[31m ***** adding application user ***** \e[0m"
-useradd ${app_user}
-
-echo -e "\e[31m ***** creating the app directory ***** \e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[31m ***** downloading and unzipping the application code  ***** \e[0m"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-cd /app
-unzip /tmp/dispatch.zip
+func_app_prereq
 
 cd /app
 
-echo -e "\e[31m ***** Download the dependencies ***** \e[0m"
+func_print_head "Download the dependencies"
 go mod init dispatch
 go get
 go build
 
-echo -e "\e[31m ***** setup systemD dispatch service ***** \e[0m"
-cp ${script_path}//dispatch.service /etc/systemd/system/dispatch.service
-
-echo -e "\e[31m ***** start and enable dispatch service ***** \e[0m"
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl restart dispatch
+func_systemd_setup
