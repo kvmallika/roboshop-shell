51b5990a3cfab441f7e1b3dbeb803c06e243abb9
Mallika <kmallika.malli@gmail.com>
2023-05-16 11:04:38 +0530

    changes in the frontend

--- frontend.sh
+++ frontend.sh
@@ -2,14 +2,29 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-yum install nginx -y
+func_print_head "Install nginx"
+yum install nginx -y &>>$log_file
+func_stat_check $?
 
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+func_print_head "coping the config. file"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+func_stat_check $?
 
+func_print_head "Clean old app content"
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+func_stat_check $?
 
+func_print_head "Downloading' the app content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+func_stat_check $?
+
+func_print_head "Extracting the app content"
 cd  /usr/share/nginx/html
-unzip /tmp/frontend.zip
+func_stat_check $?
+unzip /tmp/frontend.zip &>>$log_file
+func_stat_check $?
+
+func_print_head "enable and start the systemd service"
 systemctl enable nginx
 systemctl restart nginx
+func_stat_check $?
